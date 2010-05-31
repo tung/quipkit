8ec22fafd6cd296750ff22c3a1d6a170a18ed113
Tung Nguyen <tunginobi@gmail.com>
2010-05-31 20:05:25 +1000

    Test PNG loading with libpng bindings. Closes #8.

--- test/pngtest.lua
+++ test/pngtest.lua
@@ -1,2 +1,11 @@
 require "png"
-print("PNG loads without errors")
+
+image = PNG.Open("test/smile.png")
+
+print("width = " .. image.width)
+print("height = " .. image.height)
+if image.format == PNG.RGB then
+	print("format = RGB")
+else
+	print("format = RGBA")
+end
