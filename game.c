791cc1b2dcb41f79e02591afb930f64822c295e6
Tung Nguyen <tunginobi@gmail.com>
2010-06-15 12:57:19 +1000

    Refactor Quipkit engine code.

--- game.c
+++ game.c
@@ -1,20 +1,24 @@
-#include <stdio.h>
-
 #include <lua.h>
 #include <lauxlib.h>
 #include <lualib.h>
+#include <stdio.h>
+
+const char *INIT_SCRIPT = "init.lua";
+const char *MAIN_SCRIPT = "main.lua";
+
+
 
 int main(int argc, char *argv[]) {
     lua_State *L = (lua_State *)lua_open();
     luaL_openlibs(L);
 
-    if (luaL_loadfile(L, "init.lua") || lua_pcall(L, 0, 0, 0)) {
+    if (luaL_loadfile(L, INIT_SCRIPT) || lua_pcall(L, 0, 0, 0)) {
         fprintf(stderr, "Init script failed: %s\n", lua_tostring(L, -1));
         lua_close(L);
         return 1;
     }
 
-    const char *main_script = "main.lua";
+    const char *main_script = MAIN_SCRIPT;
     if (argc > 1) {
         main_script = argv[1];
     }
