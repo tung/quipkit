3b69f9ea3d42653d702ad3095f746e1a0cb2d201
Tung Nguyen <tunginobi@gmail.com>
2010-09-06 23:40:26 +1000

    Compile luagl.dll on Windows. Goes towards #65.
     * luaglu.dll coming soon.

--- lib/gl.lua
+++ lib/gl.lua
@@ -1,6 +1,6 @@
 local path
 if WIN32 then
-    path = "lib/gl/luagl1_3_Win32_dll8_lib/luagl.dll"
+    path = "lib/gl/luagl/luagl.dll"
 else
     -- Assume Linux for now.
     path = "lib/gl/luagl/libluagl.so"
