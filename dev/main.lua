ae29e2a0c065585597bdf03b2603825667eb7f10
Tung Nguyen <tunginobi@gmail.com>
2010-09-18 23:36:44 +1000

    Move editor test into new test directory.

--- dev/main.lua
+++ dev/main.lua
@@ -1,12 +1,1 @@
-require "ltk"
-
-hello = ltk.button{
-    text = "Hello, world!",
-    command = function ()
-        print("Hello, world!")
-        ltk.exit()
-    end
-}
-ltk.pack{ hello }
-
-ltk.mainloop()
+dofile("dev/test/hello.lua")
