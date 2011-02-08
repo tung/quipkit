6b1fcf963d0aa63ad7d2ca27d1b6c926db4df72c
Tung Nguyen <tunginobi@gmail.com>
2011-02-08 18:33:48 +1100

    Remove engine debugging output

--- src/game.c
+++ src/game.c
@@ -7,18 +7,6 @@
 #include "options.h"
 
 
-static void DumpOptions(const char *label, const opt_Options *opts) {
-    printf("%s:\n", label);
-    if (opts->has_script) { printf("script: %s\n", opts->script); }
-    if (opts->has_config) { printf("config: %s\n", opts->config); }
-    if (opts->has_base) { printf("base: %s\n", opts->base); }
-    if (opts->has_width) { printf("width: %d\n", opts->width); }
-    if (opts->has_height) { printf("height: %d\n", opts->height); }
-    if (opts->has_fullscreen) { printf("fullscreen: %d\n", opts->fullscreen); }
-    if (opts->has_channels) { printf("channels: %d\n", opts->channels); }
-}
-
-
 static int GetConfig(int argc, char *argv[], opt_Options *cmd_line_opts, opt_Options *config_opts, int *script_args_start) {
     int po_ret = opt_ParseOptions(argc, argv, cmd_line_opts, script_args_start);
     if (po_ret) {
@@ -26,21 +14,11 @@ static int GetConfig(int argc, char *argv[], opt_Options *cmd_line_opts, opt_Opt
         if (po_ret == -1) { return -1; }
         return 1;
     }
-    DumpOptions("Command line options", cmd_line_opts);
 
     int lc_ret = opt_LoadConfig(cmd_line_opts, config_opts);
     if (lc_ret) {
         printf("%s\n", opt_GetError());
         return 1;
-    }
-    DumpOptions("Config options", config_opts);
-
-    if (*script_args_start < argc) {
-        printf("Script args:");
-        for (int i = *script_args_start; i < argc; ++i) {
-            printf(" %s", argv[i]);
-        }
-        printf("\n");
     }
 
     return 0;
