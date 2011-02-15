fbd1ac3c78e314b2f97c39d4cf2008dcdc7929d9
Tung Nguyen <tunginobi@gmail.com>
2011-02-15 23:37:32 +1100

    Simplify package.path augment in api.lua
     * Old code worked even if package.path was empty, but the chance of
       that is virtually nil, hence the change.

--- api.lua
+++ api.lua
@@ -2,12 +2,7 @@
 
 -- Add path of library bootstrapping scripts to package.path.
 local old_package_path = package.path
-local package_path_list = {}
-for part in string.gmatch(package.path, "[^%;]+") do
-    table.insert(package_path_list, part)
-end
-table.insert(package_path_list, 1, "./lib/?.lua")
-package.path = table.concat(package_path_list, ";")
+package.path = './lib/?.lua;' .. package.path
 
 -- Detect Windows.
 rawset(_G, "WIN32", os.getenv("WINDIR") ~= nil)
